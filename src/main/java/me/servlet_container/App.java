6c3d862691f5030c7234470eb63f465a7b8d70c8
sombrero104 <sombrero.khy@gmail.com>
2021-04-06 23:17:06 +0900

    .

---
+++ src/main/java/me/servlet_container/App.java
@@ -0,0 +1,13 @@
+package me.servlet_container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ src/main/java/me/servlet_container/Server_0.java
@@ -0,0 +1,45 @@
+package me.servlet_container;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server_0 {
+
+    private ServerSocket serverSocket;
+
+    private void boot() throws IOException {
+        serverSocket = new ServerSocket(8000); // http://localhost:8000
+        Socket socket = serverSocket.accept();
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+        int oneInt = -1;
+        while(-1 != (oneInt = in.read())) {
+            System.out.print((char)oneInt);
+        }
+        out.close();
+        in.close();
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server_0 server_0 = new Server_0();
+        server_0.boot();
+    }
+
+    /**
+     * 출력 결과:
+     *
+     *      GET / HTTP/1.1
+     *      Host: localhost:8000
+     *      Upgrade-Insecure-Requests: 1
+     *      Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*⁄*;q=0.8
+     *      User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15
+     *      Accept-Language: ko-kr
+     *      Accept-Encoding: gzip, deflate
+     *      Connection: keep-alive
+     */
+
+}
