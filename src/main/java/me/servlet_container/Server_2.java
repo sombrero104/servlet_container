e260710c26da678cadfd38fbc5d5dc749cc21f3f
sombrero104 <sombrero.khy@gmail.com>
2021-04-07 11:22:31 +0900

    .

--- src/main/java/me/servlet_container/Server_2.java
+++ src/main/java/me/servlet_container/Server_2.java
@@ -87,11 +87,11 @@ public class Server_2 {
             System.out.printf("   Key: %s Value: %s\n", headerName, headerMap.get(headerName));
         }
         if(bodyByteList != null) {
-            System.out.print("Message Body -->");
+            System.out.print("Message Body --> ");
             for(byte oneByte : bodyByteList) {
                 System.out.print(oneByte);
             }
-            System.out.println("<--");
+            System.out.println(" <--");
         }
         System.out.println("End of HTTP Message.");
     }
@@ -106,6 +106,7 @@ public class Server_2 {
      *
      * 출력 결과:
      *
+     * 1. GET 요청일 경우.
      *      METHOD: GET REQ: / HTTP VER. HTTP/1.1
      *      Header list
      *          Key: Accept Value:  text/html,application/xhtml+xml,application/xml;q=0.9,*⁄*;q=0.8
@@ -115,6 +116,50 @@ public class Server_2 {
      *          Key: Host Value:  localhost:8000
      *          Key: Accept-Language Value:  ko-kr
      *          Key: Accept-Encoding Value:  gzip, deflate
+     *      End of HTTP Message.
+     *
+     * 2. POST 요청일 경우.
+     * (1) 사파리 브라우저에서 요청한 경우.
+     *      METHOD: POST REQ: / HTTP VER. HTTP/1.1
+     *      Header list
+     *          Key: Origin Value:  http://localhost:63342
+     *          Key: Cookie Value:  Idea-3ef64c5c=faeb596a-446d-465a-98aa-aa388ba67073
+     *          Key: Accept Value:  text/html,application/xhtml+xml,application/xml;q=0.9,*⁄*;q=0.8
+     *          Key: Upgrade-Insecure-Requests Value:  1
+     *          Key: User-Agent Value:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15
+     *          Key: Connection Value:  keep-alive
+     *          Key: Referer Value:  http://localhost:63342/servlet_container/static/post.html?_ijt=pv6bp7mv5b8d4hf2r7ov1uqqla
+     *          Key: Host Value:  localhost:8000
+     *          Key: Accept-Encoding Value:  gzip, deflate
+     *          Key: Accept-Language Value:  ko-kr
+     *          Key: Content-Length Value:  34
+     *          Key: Content-Type Value:  application/x-www-form-urlencoded
+     *      Message Body --> 10511011211711611097109101611051101121171161189710811710138115117981091051166111511798109105116 <--
+     *      End of HTTP Message.
+     *
+     * (2) 크롬 브라우저에서 요청한 경우.
+     *      METHOD: POST REQ: / HTTP VER. HTTP/1.1
+     *      Header list
+     *          Key: Origin Value:  http://localhost:63342
+     *          Key: Cookie Value:  Idea-3ef64c5c=faeb596a-446d-465a-98aa-aa388ba67073
+     *          Key: Accept Value:  text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*⁄*;q=0.8,application/signed-exchange;v=b3;q=0.9
+     *          Key: Connection Value:  keep-alive
+     *          Key: User-Agent Value:  Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36
+     *          Key: Referer Value:  http://localhost:63342/
+     *          Key: Sec-Fetch-Site Value:  same-site
+     *          Key: Sec-Fetch-Dest Value:  document
+     *          Key: Host Value:  localhost:8000
+     *          Key: Accept-Encoding Value:  gzip, deflate, br
+     *          Key: Sec-Fetch-Mode Value:  navigate
+     *          Key: sec-ch-ua Value:  "Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"
+     *          Key: sec-ch-ua-mobile Value:  ?0
+     *          Key: Cache-Control Value:  max-age=0
+     *          Key: Upgrade-Insecure-Requests Value:  1
+     *          Key: Sec-Fetch-User Value:  ?1
+     *          Key: Accept-Language Value:  ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
+     *          Key: Content-Length Value:  34
+     *          Key: Content-Type Value:  application/x-www-form-urlencoded
+     *      Message Body --> 10511011211711611097109101611051101121171161189710811710138115117981091051166111511798109105116 <--
      *      End of HTTP Message.
      *
      */
